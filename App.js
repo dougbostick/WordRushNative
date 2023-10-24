ee647c792b83d5bd6bd0ee62a71478f2002d3f78
Doug Bostick <dougbostick923@gmail.com>
2023-10-24 15:28:41 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [timer, setTimer] = useState(30);
+  const [score, setScore] = useState(0);
+  const [correct, setCorrect] = useState(false);
+  const [intervalId, setIntervalId] = useState(null);
+  const [gameStatus, setGameStatus] = useState(false);
+  const [wordLength, setWordLength] = useState('???');
+  const [firstLetter, setFirstLetter] = useState('???');
+  const [guess, setGuess] = useState('');
+  const [guessList, setGuessList] = useState({});
+  const [message, setMessage] = useState(
+    'Guess words with the correct first letter and length'
+  );
+  const [repeatedWord, setRepeatedWord] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.row}>
+        <View>
+          <Text>First Letter</Text>
+          <View style={styles.box}>
+            <Text>{firstLetter}</Text>
+          </View>
+        </View>
+        <View>
+          <Text>Timer</Text>
+          <View style={styles.box}>
+            <Text>{timer}</Text>
+          </View>
+        </View>
+        <View>
+          <Text>Word Length</Text>
+          <View style={styles.box}>
+            <Text>{wordLength}</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View>
+          <Text>Score</Text>
+          <View style={correct ? styles.score : styles.box}>
+            <Text>{score}</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    // text-align: 'center',
+    // height: 100,
     flex: 1,
-    backgroundColor: '#fff',
+    border: '1px solid black',
+    color: 'black',
+    marginTop: 0,
+    display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
 });
